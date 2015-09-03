b1cf3ac13ab186e90a5c708bc818085987da8def
Gabriel Adomnicai <gabesoft@gmail.com>
2015-09-02 18:48:23 -0700

    Evil snipe settings

--- contrib/!vim/evil-snipe/packages.el
+++ contrib/!vim/evil-snipe/packages.el
@@ -5,6 +5,7 @@
     :diminish evil-snipe-mode
     :init
     (setq evil-snipe-scope 'whole-buffer
+          evil-snipe-repeat-scope 'whole-buffer
           evil-snipe-enable-highlight t
           evil-snipe-enable-incremental-highlight t
           evil-snipe-auto-disable-substitute t
