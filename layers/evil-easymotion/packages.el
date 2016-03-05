36ef5fa0b3a5cf4e1630bdc08aa6a40941d9c03c
Gabriel Adomnicai <gabesoft@gmail.com>
2016-03-04 20:12:34 -0800

    Spacemacs upgrade fixes

--- layers/evil-easymotion/packages.el
+++ layers/evil-easymotion/packages.el
@@ -1,30 +1,47 @@
 (setq evil-easymotion-packages '(evil-easymotion))
 
 (defun evil-easymotion/init-evil-easymotion ()
-  (use-package evil-easymotion
-    :init
-    (progn
-      (let ((prefix "g"))
-        (setq avy-keys (append
-                        (number-sequence ?a ?z)
-                        (number-sequence ?A ?Z)
-                        (list ?, ?. ?' ?-)))
-        (setq avy-style 'de-bruijn)
-        (evilem-define (kbd (concat prefix " w")) 'evil-forward-word-begin)
-        (evilem-define (kbd (concat prefix " W")) 'evil-forward-WORD-begin)
-        (evilem-define (kbd (concat prefix " e")) 'evil-forward-word-end)
-        (evilem-define (kbd (concat prefix " E")) 'evil-forward-WORD-end)
-        (evilem-define (kbd (concat prefix " b")) 'evil-backward-word-begin)
-        (evilem-define (kbd (concat prefix " B")) 'evil-backward-WORD-begin)
-        (evilem-define (kbd (concat prefix " j")) 'next-line
-                       (lambda ()
-                         (setq evil-this-type 'line))
-                       nil
-                       ((temporary-goal-column (current-column))
-                        (line-move-visual nil)))
-        (evilem-define (kbd (concat prefix " k")) 'previous-line
-                       (lambda ()
-                         (setq evil-this-type 'line))
-                       nil
-                       ((temporary-goal-column (current-column))
-                        (line-move-visual nil)))))))
+  :init
+  (progn
+    (evilem-default-keybindings "g")
+    (let ((prefix "g"))
+      (setq avy-keys (append
+                      (number-sequence ?a ?z)
+                      (number-sequence ?A ?Z)
+                      (list ?, ?. ?' ?-)))
+      (setq avy-style 'de-bruijn)
+      (evilem-define (kbd (concat prefix " w")) 'evil-forward-word-begin)
+      (evilem-define (kbd (concat prefix " W")) 'evil-forward-WORD-begin)
+      (evilem-define (kbd (concat prefix " e")) 'evil-forward-word-end)
+      (evilem-define (kbd (concat prefix " E")) 'evil-forward-WORD-end)
+      (evilem-define (kbd (concat prefix " b")) 'evil-backward-word-begin)
+      (evilem-define (kbd (concat prefix " B")) 'evil-backward-WORD-begin))))
+
+  ;; (use-package evil-easymotion
+  ;;   :defer t
+  ;;   :init
+  ;;   (progn
+  ;;     (let ((prefix "g"))
+  ;; (setq avy-keys (append
+  ;;                 (number-sequence ?a ?z)
+  ;;                 (number-sequence ?A ?Z)
+  ;;                 (list ?, ?. ?' ?-)))
+  ;;       (setq avy-style 'de-bruijn)
+  ;;       (evilem-define (kbd (concat prefix " w")) 'evil-forward-word-begin)
+  ;;       (evilem-define (kbd (concat prefix " W")) 'evil-forward-WORD-begin)
+  ;;       (evilem-define (kbd (concat prefix " e")) 'evil-forward-word-end)
+  ;;       (evilem-define (kbd (concat prefix " E")) 'evil-forward-WORD-end)
+  ;;       (evilem-define (kbd (concat prefix " b")) 'evil-backward-word-begin)
+  ;;       (evilem-define (kbd (concat prefix " B")) 'evil-backward-WORD-begin)
+  ;;       (evilem-define (kbd (concat prefix " j")) 'next-line
+  ;;                      (lambda ()
+  ;;                        (setq evil-this-type 'line))
+  ;;                      nil
+  ;;                      ((temporary-goal-column (current-column))
+  ;;                       (line-move-visual nil)))
+  ;;       (evilem-define (kbd (concat prefix " k")) 'previous-line
+  ;;                      (lambda ()
+  ;;                        (setq evil-this-type 'line))
+  ;;                      nil
+  ;;                      ((temporary-goal-column (current-column))
+  ;;                       (line-move-visual nil))))))
