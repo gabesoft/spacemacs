695165fa656480edeb361a9e50ecc3864198faad
Gabriel Adomnicai <gabesoft@gmail.com>
2016-03-25 23:30:23 -0700

    Dired settings

--- layers/dired-hacks/extensions.el
+++ layers/dired-hacks/extensions.el
@@ -9,11 +9,75 @@
 (load-file (concat user-emacs-directory "layers/dired-hacks/extensions/dash.el"))
 (load-file (concat user-emacs-directory "layers/dired-hacks/extensions/dired-hacks-utils.el"))
 (load-file (concat user-emacs-directory "layers/dired-hacks/extensions/dired-rainbow.el"))
-(load-file (concat user-emacs-directory "layers/dired-hacks/extensions/dired-subtree.el"))
+
+
+
+(defun dired-hacks-home-directory ()
+  "Open the a dired buffer in user's home directory."
+  (interactive)
+  (find-alternate-file user-home-directory))
+
+(defun dired-hacks-dotfiles-toggle ()
+  "Show/hide dot-files"
+  (interactive)
+  (when (equal major-mode 'dired-mode)
+    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+        (progn
+          (set (make-local-variable 'dired-dotfiles-show-p) nil)
+          (message "h")
+          (dired-mark-files-regexp "^\\\.")
+          (dired-do-kill-lines))
+      (progn (revert-buffer) ; otherwise just revert to re-show
+             (set (make-local-variable 'dired-dotfiles-show-p) t)))))
+
+(defun dired-hacks-up-directory (&optional other-window)
+  "Run Dired on parent directory of current directory."
+  (interactive "P")
+  (let* ((dir (dired-current-directory))
+         (orig (current-buffer))
+         (up (file-name-directory (directory-file-name dir))))
+    (or (dired-goto-file (directory-file-name dir))
+        ;; Only try dired-goto-subdir if buffer has more than one dir.
+        (and (cdr dired-subdir-alist)
+             (dired-goto-subdir up))
+        (progn
+          (kill-buffer orig)
+          (dired up)
+          (dired-goto-file dir)))))
 
 (defun dired-hacks/init-dired-hacks ()
   "Initialize the dired-hacks layer."
   (progn
+    (define-key evil-normal-state-map (kbd "-") 'dired-jump)
+    (add-hook 'dired-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map
+                  (kbd "gg") 'evil-goto-first-line)
+                (define-key evil-normal-state-local-map
+                  (kbd "G") 'evil-goto-line)
+                (define-key evil-normal-state-local-map
+                  (kbd "u") 'dired-undo)
+                (define-key evil-normal-state-local-map
+                  (kbd "-") 'dired-hacks-up-directory)
+                (define-key evil-normal-state-local-map
+                  (kbd "^") 'dired-hacks-up-directory)
+                (define-key evil-normal-state-local-map
+                  (kbd "I") 'dired-hacks-dotfiles-toggle)
+                (define-key evil-normal-state-local-map
+                  (kbd "J") 'dired-next-subdir)
+                (define-key evil-normal-state-local-map
+                  (kbd "K") 'dired-prev-subdir)
+                (define-key evil-normal-state-local-map
+                  (kbd "C-k") 'dired-do-kill-lines)
+                (define-key evil-normal-state-local-map
+                  (kbd "~") 'dired-hacks-home-directory)
+                (define-key evil-normal-state-local-map
+                  (kbd "j") 'dired-next-line)
+                (define-key evil-normal-state-local-map
+                  (kbd "k") 'dired-previous-line)
+                (define-key evil-normal-state-local-map
+                  (kbd "RET") 'dired-find-alternate-file)
+                ))
     (use-package dired-rainbow
       :init
       (progn
@@ -43,26 +107,4 @@
         (dired-rainbow-define gitignore (:inherit default :italic t) ".gitignore")
         (dired-rainbow-define gitconfig (:inherit default :italic t) ".gitconfig")
 
-        (dired-rainbow-define-chmod executable-unix "Green" "-.*x.*")))
-
-    (use-package dired-subtree
-      :init
-      (setq-default dired-subtree-use-backgrounds nil)
-      (bind-keys :map dired-mode-map
-                 :prefix ","
-                 :prefix-map dired-subtree-map
-                 :prefix-docstring "Dired subtree map."
-                 ("j" . dired-subtree-insert)
-                 ("C-/" . dired-subtree-apply-filter)
-                 ("k" . dired-subtree-remove)
-                 ("n" . dired-subtree-next-sibling)
-                 ("p" . dired-subtree-previous-sibling)
-                 ("u" . dired-subtree-up)
-                 ("d" . dired-subtree-down)
-                 ("a" . dired-subtree-beginning)
-                 ("e" . dired-subtree-end)
-                 ("c" . dired-subtree-cycle)
-                 ("C-m" . dired-subtree-mark-subtree)
-                 ("C-u" . dired-subtree-unmark-subtree)
-                 ("C-o C-f" . dired-subtree-only-this-file)
-                 ("C-o C-d" . dired-subtree-only-this-directory)))))
+        (dired-rainbow-define-chmod executable-unix "Green" "-.*x.*")))))
