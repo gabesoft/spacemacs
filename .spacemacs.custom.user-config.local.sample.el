8a2627683847995ca57805054275bf017ae66c3f
Gabriel Adomnicai <gabesoft@gmail.com>
2016-11-07 20:24:30 -0800

    Excluded local config from source control

--- .spacemacs.custom.user-config.local.sample.el
+++ .spacemacs.custom.user-config.local.sample.el
@@ -1,5 +1,14 @@
+
 (defun dotspacemacs/user-config-local ()
+  (defun set-default-font-to-monaco ()
+    "Sets the default font to Monaco 10"
+    (interactive)
+    (set-default-font "Monaco 10"))
+
+  (setq hindent-process-path (concat user-home-directory ".local/bin/hindent"))
 
   )
 
 (provide 'spacemacs-custom-user-config-local)
+
+;; (require 'spacemacs-custom-user-config-local)
